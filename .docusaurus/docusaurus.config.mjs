238d8972f1b64b769813797c0e848016beaeebb5
JuanDavid_Dev <juandavid.dev@gmail.com>
2024-11-30 20:34:50 -0500

    V.0.0.7
    Prueba del despliegue auto.

--- .docusaurus/docusaurus.config.mjs
+++ .docusaurus/docusaurus.config.mjs
@@ -4,6 +4,21 @@
  * Modify the docusaurus.config.js file at your site's root instead.
  */
 export default {
+  "scripts": [
+    {
+      "src": "https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js",
+      "async": true,
+      "data-name": "BMC-Widget",
+      "data-cfasync": "false",
+      "data-id": "juandavid",
+      "data-description": "Support me on Buy me a coffee!",
+      "data-message": "¡Aquí puedes invitarme a un Café!",
+      "data-color": "#FFBC39",
+      "data-position": "right",
+      "data-x_margin": "120",
+      "data-y_margin": "30"
+    }
+  ],
   "title": "¡En construcción!",
   "tagline": "@JuanDavid_Dev",
   "favicon": "img/jdocdev_logo.png",
@@ -369,7 +384,6 @@ export default {
   "customFields": {},
   "plugins": [],
   "themes": [],
-  "scripts": [],
   "headTags": [],
   "stylesheets": [],
   "clientModules": [],
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,6 +10,22 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
+    scripts: [
+        {
+            src: "https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js",
+            async: true,
+            "data-name": "BMC-Widget",
+            "data-cfasync": "false",
+            "data-id": "juandavid",
+            "data-description": "Support me on Buy me a coffee!",
+            "data-message": "¡Aquí puedes invitarme a un Café!",
+            "data-color": "#FFBC39",
+            "data-position": "right",
+            "data-x_margin": "120",
+            "data-y_margin": "30",
+        },
+    ],
+
     title: "¡En construcción!",
     tagline: "@JuanDavid_Dev",
     favicon: "img/jdocdev_logo.png",
